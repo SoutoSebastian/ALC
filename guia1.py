d4409bc98a819d3ea7427bba5850c757256ac790
Sebastian <soutosebastianma@gmail.com>
2024-08-18 13:48:04 -0300

    primer dia

---
+++ guia1.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot  as plt
+
+#########EJ 3
+#Vectores
+
+v = np.array([1,2,3,-1])
+w = np.array([2,3,0,5])
+
+# print(v+w)
+# print(2*v)
+# print(v**2)
+
+
+#Matrices
+
+A = np.array([[1,2,3,4,5],[0,1,2,3,4],[2,3,4,5,6],[0,0,1,2,3],[0,0,0,0,1]])
+#print(A)
+#print(A[0:2, 3:5])
+#print(A[:2, 3:])
+
+#print(A[ [ 0 , 2 , 4 ] , : ])
+ind = np.array( [ 0 , 2 , 4 ] )
+#print(A[ ind , ind ])
+#print(A[ ind , ind [ : , None ] ])
+
+#Numeros complejos
+
+# print(1j*1j)
+# print((1+2j)*1j)
+
+#########EJ 4
+# A = np.array([[1,1,1],[4,2,1],[9,3,1]])
+# b = np.array([1,2,0])
+# x = np.linalg.solve(A, b)
+
+#print(x)
+
+xx = np.array([1,2,3])
+yy = np.array([1,2,0])
+
+x = np.linspace(0,4,100)
+f = lambda t:-1.5*t**2+5.5*t-3
+
+plt.plot(xx,yy,'*')
+plt.plot(x,f(x))
+plt.show()
