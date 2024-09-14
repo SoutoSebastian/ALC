764ccc312f694f909b9c5e423d73ecf5863e4604
Sebastian <soutosebastianma@gmail.com>
2024-09-14 16:49:23 -0300

    probando LU

---
+++ Labos/elim_gaussianaConPermu.py
@@ -0,0 +1,111 @@
+"""
+Eliminacion Gausianna
+"""
+import numpy as np
+
+
+def elim_gaussianaConPermutaciones(A):
+    cant_op = 0
+    m=A.shape[0]
+    n=A.shape[1]
+    Ac = A.copy()
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return
+    
+    ## desde aqui -- CODIGO A COMPLETAR
+    L = np.eye(n) #inciamos L de nxn
+
+    matricesPermutacion=[]
+
+    print("esto es n--->",n)
+
+    #Gaussian Elimination
+    for i in range(n):
+        if Ac[i,i]==0:
+            # ...
+            # aca supongo q tengo q hacer la permutacion 
+            print(f"Pivote {i} es nulo")
+            
+            listaDePivotes:list[tuple] = []
+            for j in range(i+1,n):
+                listaDePivotes.append((Ac[j,i],j))   #listaDePivotes.append((elemento,indice)), tupla = (elemento,indice)
+
+            print(listaDePivotes)
+
+            newPivoteIndice= listaDePivotes[0]
+            
+            for k in range(len(listaDePivotes)-1):
+                    
+                if newPivoteIndice[0]<= listaDePivotes[k][0]:
+                    newPivoteIndice = listaDePivotes[k]
+                print(newPivoteIndice)
+            #ahora resta crear una identidad con las filas intercambiadas por los indices newPivote[]
+
+
+            identidad = np.eye(n)
+            permutacion = np.eye(n)     #la transformo mas abajo, arranco con la Identidad
+            permutacionFila1 = identidad[i]
+            permutacionFila2 = identidad[newPivoteIndice[1]]
+            permutacion[i] = permutacionFila2
+            permutacion[newPivoteIndice[1]]=permutacionFila1 
+            matricesPermutacion.append(permutacion) #estaran en la fila en un orden inverso al propuesto para conseguir P
+
+
+
+            Ac= permutacion @ Ac
+
+            for j in range(i+1, n):
+                factor = Ac[j,i] / Ac[i,i]#aca va q multiplico entre las matrices para q de 0.
+                L[j,i] = factor # guardamos el factor en la matriz L 
+                Ac[j,i:] = Ac[j,i:]  - factor*Ac[i,i:]
+                #cant_operaciones #... n por algo
+                print(f"Matriz L despuse del paso ({j},{i})")
+                print(L) 
+            
+            for k in range (i):
+                L[0:,k] = (permutacion @ L[0:,k])
+                print(f'L despues de permutar \n {L}')    
+
+        else:
+            for j in range(i+1, n):
+                factor = Ac[j,i] / Ac[i,i]#aca va q multiplico entre las matrices para q de 0.
+                L[j,i] = factor # guardamos el factor en la matriz L 
+                Ac[j,i:] = Ac[j,i:]  - factor*Ac[i,i:]
+                #cant_operaciones #... n por algo
+                #print(f"Matriz L despuse del paso ({j},{i})")
+                #print(L) 
+
+    P=multiplicarPermutaciones(matricesPermutacion)
+    ## hasta aqui
+            
+    #L = np.tril(Ac,-1) + np.eye(A.shape[0]) 
+    print("esto queda despues de todos los pasos --->\n",Ac)
+    U = np.triu(Ac)
+    
+
+
+    return L, U, cant_op, P
+
+def multiplicarPermutaciones(matricesPermutaciones:list):
+    P=matricesPermutaciones[len(matricesPermutaciones)-1]
+    for i in range(len(matricesPermutaciones)-2,-1,-1):         #el menos 1 del segundo item es porque es -1 exclusive
+        Pnmenos1 = matricesPermutaciones[i]
+        P = P @ Pnmenos1
+
+    return P
+
+
+matrizEjemplo = np.array([[1,2,3,4],[1,2,5,6],[1,3,5,2],[1,3,5,4]])
+matrizEjemplo2 = np.array([[1,2,3],[1,2,5],[3,4,1]])
+matrizEjemplo3 = np.array([[0,2,1,3,4],[1,-1,2,0,1],[0,0,0,1,-1],[2,0,3,4,2],[3,1,4,2,0]])
+matrizEjemplo4 = np.array([[0,2,1,3,4,5],[1,-1,2,0,1,2],[0,0,0,1,-1,3],[2,0,3,4,2,1],[0,0,1,2,0,1],[3,1,4,2,0,0]])
+
+L, U, cant_op, P = elim_gaussianaConPermutaciones(matrizEjemplo4)
+print(f'P:{P}')
+print(f'L:{L}')
+print(f'U:{U}')
+print(P@matrizEjemplo4)
+print(L@U)
+
